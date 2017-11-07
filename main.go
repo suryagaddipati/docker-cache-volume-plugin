3a96b108fad59de7e5688bf5c166e9e8c4d8057d
Surya Gaddipati <sgaddipati@groupon.com>
2017-11-07 15:48:37 -0600

    skeleton implementation

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 import(
   "os"
+  "log"
   "strconv"
   "github.com/Sirupsen/logrus"
-  "github.com/docker/go-plugins-helpers/volume"
+	"github.com/docker/go-plugins-helpers/volume"
 )
+const socketAddress = "/run/docker/plugins/cache-driver.sock"
+
 func main() {
   debug := os.Getenv("DEBUG")
 	if ok, _ := strconv.ParseBool(debug); ok {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
+  lowerRootDir := "/cache"
+	upperRootDir := "/mnt/cache-upper"
+	workRootDir := "/mnt/cache-work"
+	mergedRootDir :=  "/mnt/cache-merged"
+
+  d, err := newCacheDriver(lowerRootDir,upperRootDir,workRootDir,mergedRootDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	h := volume.NewHandler(d)
+	logrus.Infof("listening on %s", socketAddress)
+	logrus.Error(h.ServeUnix(socketAddress, 0))
 }
